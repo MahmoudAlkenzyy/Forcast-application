673709031cfbe0ca9b8b99cdd1cae17bf69bc98f
MahmoudAlkenzyy <105129954+MahmoudAlkenzyy@users.noreply.github.com>
2023-12-30 21:33:23 +0200

    # Get Data from api

--- js/main.js
+++ js/main.js
@@ -4,4 +4,16 @@ const cityTemp = document.querySelector('.cityTemp');
 const cityImgState = document.querySelector('.cityImgState');
 const forcastScroll = document.querySelector('.forcastScroll');
 
-//featch date from api
+//fetch date from api
+let forecastDays = [];
+async function getData() {
+    const res = await fetch(
+        'http://api.weatherapi.com/v1/forecast.json?key=5b18c12fbe1b4ac3b6a154430232912&q=cairo&days=5&aqi=no&alerts=no'
+        //
+    );
+    const data = await res.json();
+    // console.log(data.forecast.forecastday);
+    forecastDays = await data.forecast.forecastday;
+    cityWeather();
+}
+getData();
