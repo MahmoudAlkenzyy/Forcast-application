ea679459915eefd6d4027dc7bdf158ad8b923981
MahmoudAlkenzyy <105129954+MahmoudAlkenzyy@users.noreply.github.com>
2023-12-30 21:31:36 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const cityName = document.querySelector('.cityName');
+const chanceRain = document.querySelector('.chanceRain');
+const cityTemp = document.querySelector('.cityTemp');
+const cityImgState = document.querySelector('.cityImgState');
+const forcastScroll = document.querySelector('.forcastScroll');
+
+//featch date from api
